fa7ddade961561e01d1dcdb33b3ac9e77d20fc78
Neuromancer <telles.dajello@gmail.com>
2020-03-11 03:16:55 -0300

    segundo commit. modificação usando jupyter

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,12 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+print(email_one)
+
+def censor1(phrase):
+  if phrase in email_one:
+    email_one_censored = email_one.replace(phrase, "CENSORED")
+  return email_one_censored
+
+print(censor1("learning algorithms"))ju
